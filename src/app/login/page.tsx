beb55c6c5f71d31ce5513fc4b11335805f515f8f
IgorRS-2302 <igor.siqueira2302@gmail.com>
2024-04-29 17:25:30 -0300

    updates made previously

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Aqui é o Login</h1>
+      <Link href="/">Voltar para tela inicial</Link>
+    </div>
+  );
+}
